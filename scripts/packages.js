16bd2d022367feac0d356d6cf570eb48ef273e4c
wifislax678 <109185236+wifislax678@users.noreply.github.com>
2022-08-06 15:21:40 +0800

    Update packages.js

--- scripts/packages.js
+++ scripts/packages.js
@@ -13,18 +13,18 @@ module.exports = [
     name: 'luci-app-argon-config',
     desc: 'argon主题设置',
   },
-  {
-    name: 'luci-app-store',
-    desc: '应用商店',
-  },
+//  {
+//    name: 'luci-app-store',
+//    desc: '应用商店',
+//  },
   {
     name: 'luci-app-ramfree',
     desc: '释放内存',
   },
-  {
-    name: 'luci-app-wol',
-    desc: 'wol 网络唤醒',
-  },
+//  {
+//    name: 'luci-app-wol',
+//    desc: 'wol 网络唤醒',
+//  },
   {
     name: 'luci-app-diskman',
     desc: '磁盘管理工具',
@@ -33,34 +33,34 @@ module.exports = [
     name: 'luci-app-smartdns',
     desc: 'smartdns DNS防污染',
   },
-  {
-    name: 'luci-app-openclash',
-    desc: 'clash的图形代理软件',
-  },
-  {
-    name: 'luci-app-aria2',
-    desc: 'aria2下载工具',
-  },
-  {
-    name: 'luci-app-zerotier',
-    desc: 'ZeroTier内网穿透',
-  },
-  {
-    name: 'luci-app-turboacc',
-    desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
-  },
-  {
-    name: 'luci-app-xlnetacc',
-    desc: '迅雷快鸟',
-  },
-  {
-    name: 'luci-app-appfilter',
-    desc: '应用过滤'
-  },
-  {
-    name: 'luci-app-cpufreq',
-    desc: 'CPU 性能优化调节',
-  },
+//  {
+//    name: 'luci-app-openclash',
+//    desc: 'clash的图形代理软件',
+//  },
+//  {
+//    name: 'luci-app-aria2',
+//    desc: 'aria2下载工具',
+//  },
+//  {
+//    name: 'luci-app-zerotier',
+//    desc: 'ZeroTier内网穿透',
+//  },
+ // {
+ //   name: 'luci-app-turboacc',
+ //   desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
+//  },
+//  {
+//    name: 'luci-app-xlnetacc',
+//    desc: '迅雷快鸟',
+//  },
+//  {
+//    name: 'luci-app-appfilter',
+//    desc: '应用过滤'
+//  },
+//  {
+//    name: 'luci-app-cpufreq',
+//    desc: 'CPU 性能优化调节',
+//  },
   // {
   //   name: 'luci-app-samba4',
   //   desc: 'samba4文件共享',
@@ -85,15 +85,15 @@ module.exports = [
   //   name: 'luci-app-syncdial',
   //   desc: '多拨虚拟网卡（需要光猫支持多拨）',
   // },
-  // {
-  //   name: 'luci-app-passwall',
-  //   desc: 'passwall代理软件',
-  // },
-  // {
-  //   name: 'luci-app-ssr-plus',
-  //   desc: 'ssr-plus 代理软件',
-  // },
-  // {
+   {
+     name: 'luci-app-passwall',
+     desc: 'passwall代理软件',
+   },
+   {
+     name: 'luci-app-ssr-plus',
+     desc: 'ssr-plus 代理软件',
+   },
+ //  {
   //   name: 'luci-app-aliddns',
   //   desc: '阿里云ddns插件',
   // },
@@ -106,8 +106,8 @@ module.exports = [
   //   desc: '依IP地址限速',
   // },
   // {
-  //   name: 'luci-app-filetransfer',
-  //   desc: '文件传输',
+   //  name: 'luci-app-filetransfer',
+ //    desc: '文件传输',
   // },
   // {
   //   name: 'luci-app-ddnsto',
@@ -125,18 +125,18 @@ module.exports = [
   //   name: 'luci-app-wrtbwmon',
   //   desc: '实时流量监测',
   // },
-  {
-    name: 'luci-app-serverchan',
-    desc: '微信/Telegram推送',
-  },
-  {
-    name: 'luci-app-transmission',
-    desc: 'BT下载工具',
-  },
-  {
-    name: 'luci-app-dailybonus',
-    desc: '京东签到服务',
-  },
+ // {
+ //   name: 'luci-app-serverchan',
+//    desc: '微信/Telegram推送',
+//  },
+//  {
+//    name: 'luci-app-transmission',
+//    desc: 'BT下载工具',
+//  },
+//  {
+//    name: 'luci-app-dailybonus',
+//    desc: '京东签到服务',
+//  },
   // {
   //   name: 'luci-app-frpc',
   //   desc: 'frpc 网络代理',
